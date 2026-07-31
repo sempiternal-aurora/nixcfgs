f169aaa2ce6e33ccdc00ae723e8730edc8637737
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-07-31 14:29:33 +1000

    afp: update and fix AutoCorres build

--- pkgs/afp/package.nix
+++ pkgs/afp/package.nix
@@ -1,33 +1,48 @@
 {
+  lib,
+  pkgsHostTarget,
   stdenvNoCC,
   isabelle,
-  fetchurl,
+  fetchFromGitLab,
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "AFP";
-  version = "2025-1-unstable-2026-03-09";
+  version = "2025-2-unstable-2026-07-21";
 
-  src = fetchurl {
-    url = "https://foss.heptapod.net/isa-afp/afp-2025-1/-/archive/4d71ef8d93bada9fe604ae7a0bea2f42030bd82d/afp-2025-1-4d71ef8d93bada9fe604ae7a0bea2f42030bd82d.tar.gz";
-    hash = "sha256-ePHSevyhtpd5GpE8z0OLCicqQMJhuUx2PpJZtAXHlSA=";
+  src = fetchFromGitLab {
+    domain = "foss.heptapod.net";
+    owner = "isa-afp";
+    repo = "afp-2025-2";
+    rev = "12aae4b0ed0d842d92ea7edafdb03e4cf0aa8911";
+    hash = "sha256-88c5ZCUcMn0ne9xnQt8lzjtfT2xQggKxVLpVDCDVA/Y=";
   };
 
-  buildPhase = ''
-    export HOME=$TMP
-    isabelle components -u $(pwd)/thys
+  postPatch = ''
+    substituteInPlace thys/AutoCorres2/c-parser/isar_install.ML \
+      --replace-fail '/usr/bin/cpp' '${lib.getExe' pkgsHostTarget.clang "cpp"}' \
+      --replace-fail '/usr/bin/clang' '${lib.getExe' pkgsHostTarget.clang "clang"}'
+
+    substituteInPlace thys/AutoCorres2/c-parser/CTranslationSetup.thy \
+      --replace-fail 'mllex "StrictC.lex"' "" \
+      --replace-fail 'mlyacc "StrictC.grm"' ""
   '';
 
   nativeBuildInputs = [ isabelle ];
 
   installPhase = ''
+    runHook preInstall
+
     dir=$out/Isabelle${isabelle.version}/contrib/${finalAttrs.pname}-${finalAttrs.version}
     mkdir -p $dir
     cp -r thys/* $dir/
+
+    runHook postInstall
   '';
 
   meta = {
     description = "Archive of Formal Proofs";
     homepage = "https://isa-afp.org";
+    platforms = isabelle.meta.platforms;
   };
 })

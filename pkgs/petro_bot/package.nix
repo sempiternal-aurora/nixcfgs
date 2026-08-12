030c1570c6b6586b4192958e40b8f35f9c21d039
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-08-13 01:24:08 +1000

    petro_bot: 0-unstable-2026-04-12 -> 0-unstable-2026-07-15

--- pkgs/petro_bot/package.nix
+++ pkgs/petro_bot/package.nix
@@ -4,14 +4,14 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "petro_bot";
-  version = "0-unstable-2026-04-12";
+  version = "0-unstable-2026-07-15";
 
   src = fetchFromGitHub {
     owner = "PETR0-4LT";
     repo = "petro_bot";
-    rev = "f113a1765ae5d3d9f7d9027b0ded6310b518d122";
-    hash = "sha256-iz7rESguK+bp5WHgKYbXJ+zRdCfV9KVzEmBtMzMFqL4=";
+    rev = "5f01212ac19859c344dd229faa8a33809c466dbe";
+    hash = "sha256-SNct8JxArExmv8AMmFoAFngB8zblwBmtzznreVZVg98=";
   };
 
-  cargoHash = "sha256-UYdbUYuWyJjQY7nZNDNOcVkrn/IfICYgApojqjjdFdM=";
+  cargoHash = "sha256-yByRIC2EZFgkQrEvuu7tf9QhEk91YxgQN95ZXveNdtY=";
 }

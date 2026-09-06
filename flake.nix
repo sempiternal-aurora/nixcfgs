c04eb6c6389940068f29c283df331df85332b86f
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-09-07 01:34:33 +1000

    n-a-s: use personal repo for latest u-boot

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
     };
 
     nixos-apple-silicon = {
-      url = "github:nix-community/nixos-apple-silicon";
+      url = "github:sempiternal-aurora/nixos-apple-silicon";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

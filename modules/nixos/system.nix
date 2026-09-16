154fbff1a506bbb81ecdca1d508f3769323a4e42
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-09-16 18:18:55 +1000

    format

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -66,13 +66,14 @@
     #   };
     # };
 
-    environment.enableAllTerminfo = true;
-
-    environment.sessionVariables = {
-      XDG_CACHE_HOME = "$HOME/.cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME = "$HOME/.local/share";
-      XDG_STATE_HOME = "$HOME/.local/state";
+    environment = {
+      enableAllTerminfo = true;
+      sessionVariables = {
+        XDG_CACHE_HOME = "$HOME/.cache";
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_DATA_HOME = "$HOME/.local/share";
+        XDG_STATE_HOME = "$HOME/.local/state";
+      };
     };
   };
 }
